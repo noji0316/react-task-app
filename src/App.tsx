978801c3dadf2c108817e16d9013cd4db39e3869
Caleb <njh2694@gmail.com>
2024-11-04 16:56:44 +0900

    전역 스타일 생성

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { appContainer, board, buttons } from './App.css'
+
+
+function App() {
+
+
+  return (
+    <div className={appContainer}>
+      <div className={board}>
+
+      </div>
+
+
+      <div className={buttons}>
+        <button>
+          이 게시판 삭제하기
+        </button>
+        <button>
+
+        </button>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
